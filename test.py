8d601380cb045645da9d09f552477a3d526744ba
luckychess <toobwn@gmail.com>
2016-11-23 13:56:09 +0300

    Eyes detection code was refactored: put indide the class and divided by methods.

--- test.py
+++ test.py
@@ -1,21 +1,33 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('/usr/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('/usr/share/OpenCV/haarcascades/haarcascade_eye.xml')
+class Detector(object):
+    """Detecting eyes and pupils"""
+    def __init__(self, img):
+        self.img = img
 
-img = cv2.imread('test.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    def detectEyes(self):
+        face_cascade = cv2.CascadeClassifier('/usr/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+        eye_cascade = cv2.CascadeClassifier('/usr/share/OpenCV/haarcascades/haarcascade_eye.xml')
+    
+        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+        for (x,y,w,h) in faces:
+            cv2.rectangle(self.img,(x,y),(x+w,y+h),(255,0,0),2)
+            self.roi_gray = gray[y:y+h, x:x+w]
+            self.roi_color = img[y:y+h, x:x+w]
+            self.eyes = eye_cascade.detectMultiScale(self.roi_gray)
+   
+    def showPicture(self):
+        for (ex,ey,ew,eh) in self.eyes:
+            cv2.rectangle(self.roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        cv2.imshow('Eyes', self.img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    img = cv2.imread('test.jpg')
+    pupilsDetector = Detector(img)
+    pupilsDetector.detectEyes()
+    pupilsDetector.showPicture()
